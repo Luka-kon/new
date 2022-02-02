976ce5e16e95512d1ce808c8b0933e6a2c0b65b7
Luka <worldlucky70@gmail.com>
2022-02-02 12:43:35 +0200

    вроде, всё version

--- function.php
+++ function.php
@@ -1,5 +1,8 @@
 <?php
 require_once ("connect.php");
+$fields = $mysqli->query("SELECT * FROM fields");
+
+
 
 function getTable($mysqli, $id="", $fio_doc="", $year_b="", $year_d="", $medwork="", $discipline="", $comment=""){
     $search="";
@@ -25,11 +28,16 @@ function getTable($mysqli, $id="", $fio_doc="", $year_b="", $year_d="", $medwork
     if ($medwork!= ""){
         if ($medwork == "Працював"){
             $medwork = 1;
+            $search=$search." AND `medwork` LIKE '%".$medwork."%'";
         }
         if ($medwork == "Не працював"){
             $medwork = 0;
+            $search=$search." AND `medwork` LIKE '%".$medwork."%'";
         }
-        $search=$search." AND `medwork` LIKE '%".$medwork."%'";
+        if ($medwork == "Всі"){
+
+        }
+
 
     }
 
@@ -52,6 +60,50 @@ function getTable($mysqli, $id="", $fio_doc="", $year_b="", $year_d="", $medwork
 
 }
 
+
+function printTable($mysqli, $query){
+    $fields = $mysqli->query("SELECT * FROM fields");
+    foreach ($fields as $tab) {
+        if ($tab["field"] !== "id") {
+            echo "<th>" . $tab["describe_ua"] . "</th>";
+        }
+        else
+        {
+            echo "<th style='width: 50px;'>№</th>";
+        }
+
+    }
+    echo "<th>Удаление</th>";
+
+
+
+    foreach ($query as $doctor) {
+        foreach ($fields as $field){
+            $fiel = $field["field"];
+            if ($fiel == "id"){
+                echo '<tr data-id="' . $doctor["$fiel"] . '">';
+                $num++;
+                echo "<td style='width: 50px;'>$num</td>";
+            }
+            elseif ($fiel == "medwork"){
+                if ($doctor["$fiel"] == 1){
+                    echo "<td data-filt='" . $fiel . "'>Працював</td>";
+                }
+                else{
+                    echo "<td data-filt='" . $fiel . "'>Не працював</td>";
+                }
+            }
+
+            else
+            {
+                echo "<td data-filt='" . $fiel . "'>" . nl2br($doctor["$fiel"]) . "</td>";
+            }
+        }
+        echo "<td ><button class='remove_sql' value=" . $doctor["id"] . " type='button'>X</button></td>";
+        echo "</tr>";
+    }
+
+}
 
 
 
--- index.php
+++ index.php
@@ -1,14 +1,9 @@
-
 <!--               ПОДКЛЮЧЕНИЕ-->
 <?php
 require_once ("connect.php");
 $fields = $mysqli->query("SELECT * FROM fields");
 $doctors = $mysqli->query("SELECT * FROM doctors");
-
 require_once ("function.php");
-if (isset($_GET['page'])){
-    echo $_GET['page'];
-}
 ?>
 
 <!--              ВЫВОД ФОРМЫ ПОИСКА JS-->
@@ -25,56 +20,50 @@ if (isset($_GET['page'])){
 <div class="row">
 <div style="margin-left: 50px;"  class="col">
     <h2>Форма пошуку</h2>
-
-
-
    <form style="" method="post">
-
-    <input name="fio_doc" placeholder="ПІБ"/> <br/>
-    <input name="year_b" placeholder="Рік народження"/> <br/>
-    <input name="year_d" placeholder="Рік смерті"/> <br/>
-    <input name="medwork" placeholder="Працював в медині?"/> <br/>
-    <input name="discipline" placeholder="Дисципліна"/> <br/>
-    <input name="comment" placeholder="Коментар"/> <br/><br/>
-    <input name="search_but" type="submit" value="Відправити запит"/><br/>
-
-    </form>
-
+       <?php
+       foreach ($fields as $tab) {
+           if ($tab["field"] != "medwork"){
+               if ($tab["field"] != "id"){
+               echo '<div class="filtform"><input name="' . $tab["field"] . '" placeholder="' . $tab["describe_ua"] . '" type="text" />   </div>';
+           }}
+           else echo '<div class="filtform"><select style="width: 50%" name="' . $tab["field"] . '" placeholder="' . $tab["describe_ua"] . '" type="text" >   
+           <option>Всі</option>
+           <option>Працював</option>
+           <option>Не працював</option>
+</select>
+</div>';
+       }
+       echo '<input name="search_but" type="submit" value="Відправити запит"/><br/>';
+       ?>
+   </form>
 </div>
+
 <div class="col">
     <br/><br/><br/>
     <form method="post">
         <input name="ubiley" type="submit" value="Передивитись юбілярів"/>
     </form>
+
 </div>
     <div class="filtforms col" >
         <h2>Форма фільтрації</h2>
     <?php
-
         foreach ($fields as $tab) {
             if ($tab["field"] != "id"){
         echo '<div class="filtform"><input placeholder="' . $tab["describe_ua"] . '" type="text" />   </div>';
             }
         }
-    //$search="SELECT * FROM doctors";
-    //$num = 0;
 ?>
 </div></div>
 
-
-
-
-
 <!--   НАЧАЛО ФОРМЫ ДОБАВЛЕНИЯ -->
-
-
-
-<table style="text-align: left">
+<table id="tab" style="text-align: left">
     <tr class="add">
         <?php
         foreach ($fields as $tab) {
             if ($tab["field"] == "id"){
-                //echo "<td></td>";
+                echo '<td><label>' . $tab["describe_ua"] . '</label></td>';
             }
             elseif ($tab["field"] == "comment" || $tab["field"] == "discipline"){
                 echo '<td><label>' . $tab["describe_ua"] . '</label><textarea data-value="' . $tab["field"]  . '"></textarea></td>';
@@ -87,126 +76,23 @@ if (isset($_GET['page'])){
         }
         echo "<td><button class='add_sql' type='button'>+</button></td>";
         ?>
-
     </tr>
-</table>
 
-
-<table>
     <?php
-
     if (isset($_POST['ubiley'])){
         $result=$mysqli->query("SELECT * FROM doctors WHERE year_b LIKE '%2'");
-
-        foreach ($fields as $tab) {
-            if ($tab["field"] !== "id") {
-                echo "<th>" . $tab["describe_ua"] . "</th>";
-            }
-            else
-            {
-                echo "<th style='width: 50px;'>№</th>";
-            }
-
-        }
-        echo "<th>Удаление</th></table>";
-        echo "<table id='tab'>";
-
-
-        foreach ($result as $doctor) {
-            foreach ($fields as $field){
-                $fiel = $field["field"];
-                if ($fiel == "id"){
-                    echo '<tr data-id="' . $doctor["$fiel"] . '">';
-                    $num++;
-                    echo "<td style='width: 50px;'>$num</td>";
-                }
-                elseif ($fiel == "medwork"){
-                    if ($doctor["$fiel"] == 1){
-                        echo "<td data-filt='" . $fiel . "'>Працював</td>";
-                    }
-                    else{
-                        echo "<td data-filt='" . $fiel . "'>Не працював</td>";
-                    }
-                }
-
-                else
-                {
-                    echo "<td data-filt='" . $fiel . "'>" . nl2br($doctor["$fiel"]) . "</td>";
-                }
-            }
-            echo "<td ><button class='remove_sql' value=" . $doctor["id"] . " type='button'>X</button></td>";
-            echo "</tr>";
-        }
-
-
+        printTable($mysqli, $result);
     }
-
     ?>
-</table>
 
-<table>
 <?php
-
-
-
-
     if (isset($_POST['search_but'])){
         $num=0;
-       $quer = getTable($mysqli, $_POST['id'], $_POST['fio_doc'], $_POST['year_b'], $_POST['year_d'], $_POST['medwork'], $_POST['discipline'], $_POST['comment']);
-
-
-        foreach ($fields as $tab) {
-            if ($tab["field"] !== "id") {
-                echo "<th>" . $tab["describe_ua"] . "</th>";
-            }
-            else
-            {
-                echo "<th style='width: 50px;'>№</th>";
-            }
-
-        }
-        echo "<th>Удаление</th></table>";
-        echo "<table id='tab'>";
-
-
-        foreach ($quer as $doctor) {
-            foreach ($fields as $field){
-                $fiel = $field["field"];
-                if ($fiel == "id"){
-                    echo '<tr data-id="' . $doctor["$fiel"] . '">';
-                    $num++;
-                    echo "<td style='width: 50px;'>$num</td>";
-                }
-                elseif ($fiel == "medwork"){
-                    if ($doctor["$fiel"] == 1){
-                        echo "<td data-filt='" . $fiel . "'>Працював</td>";
-                    }
-                    else{
-                        echo "<td data-filt='" . $fiel . "'>Не працював</td>";
-                    }
-                }
-
-                else
-                {
-                    echo "<td data-filt='" . $fiel . "'>" . nl2br($doctor["$fiel"]) . "</td>";
-                }
-            }
-            echo "<td ><button class='remove_sql' value=" . $doctor["id"] . " type='button'>X</button></td>";
-            echo "</tr>";
-        }
-
-
-
-
-
+        $quer = getTable($mysqli, $_POST['id'], $_POST['fio_doc'], $_POST['year_b'], $_POST['year_d'], $_POST['medwork'], $_POST['discipline'], $_POST['comment']);
+        printTable($mysqli, $quer);
     }
-
-
-
-
 ?>
 </table>
-
 <script src="js/jquery-3.2.0.min.js"></script>
 <script src="js/scripts.js"></script>
 </body>
